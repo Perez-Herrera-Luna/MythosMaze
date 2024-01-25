0a4f5462daad470710bc0c26f74911379006d40b
Luna Perez-Herrera <eperezherrera@nevada.unr.edu>
2024-01-24 20:58:00 -0800

    Updated player movement
    Added sprinting and crouching

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,22 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 // Based heavily on a movement controller tutorial by "Dave / Game Development"
 public class PlayerMovement : MonoBehaviour
 {
+    private float moveSpeed; // Player's movement speed
+
     [Header("Movement")]
-    public float moveSpeed; // Player's movement speed
+
+    public float walkSpeed; // Player's walk speed
+    public float sprintSpeed; // Player's run speed
 
     public float groundDrag; // Player's drag when on the ground
+
+    [Header("Jumping")]
 
     public float jumpForce; // Player's jump force
     public float jumpCooldown; // Player's jump cooldown
     public float airMultiplier; // Player's movement speed multiplier when in the air
     bool canJump;
 
+    [Header("Crouching")]
+    public float crouchSpeed; // Player's crouch speed
+    public float crouchYScale; // Player's crouch height
+    private float startYScale; // Player's starting height
+
     [Header("Keybinds")]
     public KeyCode jumpKey = KeyCode.Space; // Keybind for jumping. Hardcoded to spacebar for now
+    public KeyCode sprintKey = KeyCode.LeftShift; // Keybind for sprinting. Hardcoded to left shift for now
+    public KeyCode crouchKey = KeyCode.LeftControl; // Keybind for crouching. Hardcoded to left control for now
 
     [Header("Ground Detection")]
     public float playerHeight; // Player's height. Used for length of raycast to detect ground
@@ -31,21 +45,41 @@ public class PlayerMovement : MonoBehaviour
     Vector3 moveDirection;
     Rigidbody rb;
 
+    public MovementState state;
+
+    public enum MovementState
+    {
+        walking,
+        sprinting,
+        crouching,
+        air
+    }
+
     void Start()
     {
         rb = GetComponent<Rigidbody>();
         rb.freezeRotation = true;
 
         canJump = true;
+
+        startYScale = transform.localScale.y;
     }
 
     void Update()
     {
         // Raycast to detect if the player is on the ground
-        isGrounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 0.2f, whatIsGround);
+        if (state != MovementState.crouching)
+        {
+            isGrounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 0.2f, whatIsGround);
+        }
+        else
+        {
+            isGrounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.25f + 0.2f, whatIsGround);
+        }
 
         MyInput(); // Get player input
         SpeedControl(); // Limit player's speed
+        StateHandler(); // Handle player's movement state
 
         // Apply drag when on the ground
         if (isGrounded)
@@ -73,6 +107,40 @@ public class PlayerMovement : MonoBehaviour
             canJump = false;
             Jump();
             Invoke(nameof(ResetJump), jumpCooldown); // Reset jump cooldown
+        }
+
+        if (Input.GetKeyDown(crouchKey))
+        {
+            transform.localScale = new Vector3(transform.localScale.x, crouchYScale, transform.localScale.z);
+            rb.AddForce(Vector3.down * 5f, ForceMode.Impulse);
+        }
+
+        if (Input.GetKeyUp(crouchKey))
+        {
+            transform.localScale = new Vector3(transform.localScale.x, startYScale, transform.localScale.z);
+        }
+    }
+
+    private void StateHandler()
+    {
+        if (Input.GetKey(crouchKey))
+        {
+            state = MovementState.crouching;
+            moveSpeed = crouchSpeed;
+        }
+        else if (isGrounded && Input.GetKey(sprintKey))
+        {
+            state = MovementState.sprinting;
+            moveSpeed = sprintSpeed;
+        }
+        else if (isGrounded)
+        {
+            state = MovementState.walking;
+            moveSpeed = walkSpeed;
+        }
+        else
+        {
+            state = MovementState.air;
         }
     }
 
