f2d504383b3d1bb840a84b45589992e9db55711c
Luna Perez-Herrera <eperezherrera@nevada.unr.edu>
2024-01-28 18:55:18 -0800

    Set default values and comments
    Added default values for movement and camera script. Added additional comments to player movement script

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -10,21 +10,20 @@ public class PlayerMovement : MonoBehaviour
 
     [Header("Movement")]
 
-    public float walkSpeed; // Player's walk speed
-    public float sprintSpeed; // Player's run speed
-
-    public float groundDrag; // Player's drag when on the ground
+    public float walkSpeed = 7f; // Player's walk speed
+    public float sprintSpeed = 10f; // Player's run speed
+    public float groundDrag = 5f; // Player's drag when on the ground
 
     [Header("Jumping")]
 
-    public float jumpForce; // Player's jump force
-    public float jumpCooldown; // Player's jump cooldown
-    public float airMultiplier; // Player's movement speed multiplier when in the air
+    public float jumpForce = 7f; // Player's jump force
+    public float jumpCooldown = 0.25f; // Player's jump cooldown
+    public float airMultiplier = 0.4f; // Player's movement speed multiplier when in the air
     bool canJump;
 
     [Header("Crouching")]
-    public float crouchSpeed; // Player's crouch speed
-    public float crouchYScale; // Player's crouch height
+    public float crouchSpeed = 3.5f; // Player's crouch speed
+    public float crouchYScale = 0.5f; // Player's crouch height
     private float startYScale; // Player's starting height
 
     [Header("Keybinds")]
@@ -33,24 +32,24 @@ public class PlayerMovement : MonoBehaviour
     public KeyCode crouchKey = KeyCode.LeftControl; // Keybind for crouching. Hardcoded to left control for now
 
     [Header("Ground Detection")]
-    public float playerHeight; // Player's height. Used for length of raycast to detect ground
+    public float playerHeight = 2f; // Player's height. Used for length of raycast to detect ground
     public LayerMask whatIsGround; // Layermask for ground detection
     bool isGrounded;
 
     [Header("Slope Handling")]
-    public float maxSlopeAngle; // Maximum angle of slope the player can walk on
+    public float maxSlopeAngle = 40f; // Maximum angle of slope the player can walk on
     private RaycastHit slopeHit; // Raycast to detect slope angle
     private bool exitingSlope; // Used to detect if the player is exiting a slope
 
+    [Header("Other")]
     public Transform orientation;
+    public MovementState state;
 
     float horizontalInput;
     float verticalInput;
 
     Vector3 moveDirection;
     Rigidbody rb;
-
-    public MovementState state;
 
     public enum MovementState
     {
@@ -63,38 +62,31 @@ public class PlayerMovement : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-        rb.freezeRotation = true;
-
+        rb.freezeRotation = true; // Freeze player's rotation
         canJump = true;
-
-        startYScale = transform.localScale.y;
+        startYScale = transform.localScale.y; // Set player's starting height
     }
 
     void Update()
     {
-        // Raycast to detect if the player is on the ground
-        if (state != MovementState.crouching)
+        if (state != MovementState.crouching) // Raycast to detect if the player is on the ground
         {
             isGrounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 0.3f, whatIsGround);
         }
-        else
+        else // If the player is crouching, use a shorter raycast
         {
             isGrounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.25f + 0.3f, whatIsGround);
         }
-
-        // Debug
-        // Debug.Log(isGrounded);
 
         MyInput(); // Get player input
         SpeedControl(); // Limit player's speed
         StateHandler(); // Handle player's movement state
 
-        // Apply drag when on the ground
-        if (isGrounded)
+        if (isGrounded) // Apply drag when on the ground
         {
             rb.drag = groundDrag;
         }
-        else
+        else // Remove drag when in the air
         {
             rb.drag = 0f;
         }
@@ -110,20 +102,20 @@ public class PlayerMovement : MonoBehaviour
         horizontalInput = Input.GetAxisRaw("Horizontal");
         verticalInput = Input.GetAxisRaw("Vertical");
 
-        if (Input.GetKey(jumpKey) && isGrounded && canJump)
+        if (Input.GetKey(jumpKey) && isGrounded && canJump) // Jump if the player is on the ground and the jump key is pressed and the jump cooldown is over
         {
             canJump = false;
             Jump();
             Invoke(nameof(ResetJump), jumpCooldown); // Reset jump cooldown
         }
 
-        if (Input.GetKeyDown(crouchKey))
+        if (Input.GetKeyDown(crouchKey)) // Crouch if the crouch key is pressed
         {
             transform.localScale = new Vector3(transform.localScale.x, crouchYScale, transform.localScale.z);
             rb.AddForce(Vector3.down * 5f, ForceMode.Impulse);
         }
 
-        if (Input.GetKeyUp(crouchKey))
+        if (Input.GetKeyUp(crouchKey)) // Uncrouch if the crouch key is released
         {
             transform.localScale = new Vector3(transform.localScale.x, startYScale, transform.localScale.z);
         }
@@ -131,22 +123,22 @@ public class PlayerMovement : MonoBehaviour
 
     private void StateHandler()
     {
-        if (Input.GetKey(crouchKey))
+        if (Input.GetKey(crouchKey)) // Crouching
         {
             state = MovementState.crouching;
             moveSpeed = crouchSpeed;
         }
-        else if (isGrounded && Input.GetKey(sprintKey))
+        else if (isGrounded && Input.GetKey(sprintKey)) // Sprinting
         {
             state = MovementState.sprinting;
             moveSpeed = sprintSpeed;
         }
-        else if (isGrounded)
+        else if (isGrounded) // Walking
         {
             state = MovementState.walking;
             moveSpeed = walkSpeed;
         }
-        else
+        else // In the air
         {
             state = MovementState.air;
         }
@@ -156,35 +148,31 @@ public class PlayerMovement : MonoBehaviour
     {
         moveDirection = orientation.forward * verticalInput + orientation.right * horizontalInput;
 
-        if (OnSlope() && !exitingSlope)
+        if (OnSlope() && !exitingSlope) // Move the player on a slope
         {
             rb.AddForce(GetSlopeMoveDirection() * moveSpeed * 20f, ForceMode.Force);
 
             if (rb.velocity.y > 0)
             {
-                rb.AddForce(Vector3.down * 80f, ForceMode.Force);
+                rb.AddForce(Vector3.down * 80f, ForceMode.Force); // Apply extra force to keep the player on the ground while on slopes
             }
         }
-
-        // Apply force to the player. Variable airMultiplier is used to reduce the player's speed in the air
-        else if (isGrounded)
+        else if (isGrounded) // Move the player on flat ground
         {
             rb.AddForce(moveDirection.normalized * moveSpeed * 10f, ForceMode.Force);
         }
-        else if (!isGrounded)
+        else if (!isGrounded) // Move the player in the air. Air movement is slower than ground movement
         {
             rb.AddForce(moveDirection.normalized * moveSpeed * 10f * airMultiplier, ForceMode.Force);
         }
 
-        // turn gravity off while on a slope
-        rb.useGravity = !OnSlope();
+        rb.useGravity = !OnSlope(); // Turn gravity off while on a slope
     }
 
     // Limit the player's speed
     private void SpeedControl()
     {
-        // limiting speed on a slope
-        if (OnSlope() && !exitingSlope)
+        if (OnSlope() && !exitingSlope) // limiting speed on a slope
         {
             if (rb.velocity.magnitude > moveSpeed)
             {
@@ -206,10 +194,7 @@ public class PlayerMovement : MonoBehaviour
     private void Jump()
     {
         exitingSlope = true;
-
-        // Reset player's velocity on Y axis
-        // Ensure that the player always jumps the same height
-        rb.velocity = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
+        rb.velocity = new Vector3(rb.velocity.x, 0f, rb.velocity.z); // Reset player's velocity on Y axis. Ensures that the player always jumps the same height
 
         rb.AddForce(transform.up * jumpForce, ForceMode.Impulse);
     }
@@ -217,10 +202,10 @@ public class PlayerMovement : MonoBehaviour
     private void ResetJump()
     {
         canJump = true;
-
         exitingSlope = false;
     }
 
+    // Detect if the player is on a slope
     private bool OnSlope()
     {
         if (Physics.Raycast(transform.position, Vector3.down, out slopeHit, playerHeight * 0.5f + 0.3f))
@@ -232,6 +217,7 @@ public class PlayerMovement : MonoBehaviour
         return false;
     }
 
+    // Returns a normalized vector of the player's movement direction on a slope
     private Vector3 GetSlopeMoveDirection()
     {
         return Vector3.ProjectOnPlane(moveDirection, slopeHit.normal).normalized;
